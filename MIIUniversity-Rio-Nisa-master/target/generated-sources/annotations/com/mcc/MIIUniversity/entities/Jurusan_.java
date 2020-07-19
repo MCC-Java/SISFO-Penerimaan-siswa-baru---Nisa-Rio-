a607763d2888a052af247620cf7d500bf9fc630f
sebayang <ibrahim.sebayang@gmail.com>
2020-07-19 21:59:42 +0700

    Peace Love Gaul

---
+++ MIIUniversity-Rio-Nisa-master/target/generated-sources/annotations/com/mcc/MIIUniversity/entities/Jurusan_.java
@@ -0,0 +1,14 @@
+package com.mcc.MIIUniversity.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-07-17T08:23:28")
+@StaticMetamodel(Jurusan.class)
+public class Jurusan_ { 
+
+    public static volatile SingularAttribute<Jurusan, String> fakultas;
+    public static volatile SingularAttribute<Jurusan, String> nama;
+
+}
